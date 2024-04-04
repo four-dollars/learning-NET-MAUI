bf7b5ac28374218d58e11560bb5b10f42c378781
four-dollars <54352242+four-dollars@users.noreply.github.com>
2024-04-04 13:45:30 +0800

    phoneword

---
+++ phoneword/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿namespace Phoneword
+{
+    public partial class MainPage : ContentPage
+    {
+
+        string translatedNumber;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnTranslate(object sender, EventArgs e) //判別使用者輸入確認是否啟用撥打按鈕
+        {
+            string enteredNumber = PhoneNumberText.Text;
+            translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
+
+            if(!string.IsNullOrEmpty(translatedNumber) )
+            {
+                CallButton.IsEnabled = true;
+                CallButton.Text = "Call "+translatedNumber;
+            }
+            else
+            {
+                CallButton.IsEnabled=false;
+                CallButton.Text = "Call";
+            }
+        }
+
+        async void OnCall(object sender, EventArgs e) //非同步作業
+        {
+            if (await this.DisplayAlert (
+                "Dial a Number",
+                "Would you like to call "+translatedNumber+ "?",
+                "Yes",
+                "No")) //標題、訊息、接受、取消
+            {
+                try //PhoneDialer類別:針對各平台提供抽象的電話撥號功能
+                {
+                    if (PhoneDialer.Default.IsSupported)
+                    {
+                        PhoneDialer.Default.Open(translatedNumber); //嘗試使用電話撥號程式撥打由參數所提供的號碼
+                    }
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+                }
+                catch (Exception) 
+                {
+                    await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+                }
+                
+
+            }
+        }
+    }
+
+}
---
+++ phoneword/PhonewordTranslator.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+using System.Text;
+namespace Core;
+public static class PhonewordTranslator
+{
+    public static string? ToNumber(string raw)
+    {
+        if (string.IsNullOrWhiteSpace(raw)) return null;
+
+        raw = raw.ToUpperInvariant(); //將使用者輸入轉為大寫
+
+        var newNumber = new StringBuilder();
+        foreach (var c in raw) //尋訪使用者輸入的所有字元
+        {
+            if (" -0123456789".Contains(c)) { newNumber.Append(c); } //如果尋訪到的字元為- or 0~9，則接於字串newNumber後方
+            else
+            {
+                var result = TranslateToNumber(c); //將字元轉為對應的數字
+                if (result != null) //字元為字母
+                {
+                    newNumber.Append(result); //接於字串後方
+                }
+                else { return null; }
+            }
+        }
+        return newNumber.ToString(); //回傳值 = 轉換後的數字字串
+    }
+
+    static readonly string[] digits =
+    {
+        "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"
+    };
+
+    static int? TranslateToNumber(char c)
+    {
+        for (int i = 0; i < digits.Length; i++)
+        {
+            if (digits[i].Contains(c)) return 2 + i; //A, B, C = 2+0 = 2
+        }
+        return null;
+    }
+}
